0e0baef37ddf0936aeb0445e0442c0b3722635d4
bning <18839046851@126.com>
2019-01-02 19:33:20 +0800

    20181231

---
+++ public/yh_list.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>首页</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/yh_list.css" />
+</head>
+<body>
+    <div id="app">
+
+        <div id="img">
+            <img src="images/banner2.jpg" alt="" srcset="">
+        </div>
+        
+        
+        <div id="ptList">
+
+            <div class="card">
+                <div class="card_body">
+                    <div class="card_body_left">
+                        <img src="images/l1.png" alt="" sizes="" srcset="">
+                    </div>
+                    <div class="card_body_right">
+                        <div class="card_body_right_title">瘦脸美容针低至399元</div>
+                        <div class="card_body_right_2">
+                            <div class="card_body_right_price">
+                                <span class="card_body_right_price_old"> ￥ 899 &nbsp; </span>
+                                <br>
+                                <span class="card_body_right_price_now">￥ 399 </span>
+                            </div>
+                            <div class="card_body_right_btn"><a href="#">立即开团</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card_foot">
+                    <div class="card_body_right_time">
+                        <span class="card_body_right_time_text">距离活动结束还剩:</span> 
+                        <span class="card_body_right_time_time">1</span>
+                        <span class="card_body_right_time_text">天</span>
+                        <span class="card_body_right_time_time">12</span>
+                        <span class="card_body_right_time_text">时</span>
+                        <span class="card_body_right_time_time">3</span>
+                        <span class="card_body_right_time_text">分</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
